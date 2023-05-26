690a1935067ab7217db71d4504b8b552df698bf7
MohammadFawwaaz <MohammadFawwaaz@users.noreply.github.com>
2023-05-27 03:02:43 +0400

    Added unit tests for Validators

---
+++ src/MusicExplorer.UnitTests/Validators/ArtistReleaseRequestValidatorTests.cs
@@ -0,0 +1,133 @@
+﻿using MusicExplorer.Models.Request;
+using MusicExplorer.Validators;
+
+namespace MusicExplorer.UnitTests.Validators
+{
+    public class ArtistReleaseRequestValidatorTests
+    {
+        private readonly ArtistReleaseRequestValidator _validator;
+
+        public ArtistReleaseRequestValidatorTests()
+        {
+            _validator = new ArtistReleaseRequestValidator();
+        }
+
+        [Fact]
+        public void Validate_ValidArtistId_ShouldPass()
+        {
+            // Arrange
+            var request = new ArtistReleaseRequest
+            {
+                ArtistId = Guid.NewGuid(),
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("00000000-0000-0000-0000-000000000000")]
+        [InlineData("11111111-1111-1111-1111-111111111111")]
+        [InlineData("22222222-2222-2222-2222-222222222222")]
+        [InlineData("99999999-9999-9999-9999-999999999999")]
+        public void Validate_InvalidArtistId_ShouldFail(string artistId)
+        {
+            // Arrange
+            var request = new ArtistReleaseRequest
+            {
+                ArtistId = new Guid(artistId),
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "ArtistId");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "ArtistId must be a valid Guid separated by '-' and cannot be all 0, 1, 2, ..., or 9");
+        }
+
+        [Fact]
+        public void Validate_ValidPageNumber_ShouldPass()
+        {
+            // Arrange
+            var request = new ArtistReleaseRequest
+            {
+                ArtistId = Guid.NewGuid(),
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_InvalidPageNumber_ShouldFail()
+        {
+            // Arrange
+            var request = new ArtistReleaseRequest
+            {
+                ArtistId = Guid.NewGuid(),
+                PageNumber = 0,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "PageNumber");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "PageNumber must be greater than or equal to 1");
+        }
+
+        [Fact]
+        public void Validate_ValidPageSize_ShouldPass()
+        {
+            // Arrange
+            var request = new ArtistReleaseRequest
+            {
+                ArtistId = Guid.NewGuid(),
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_InvalidPageSize_ShouldFail()
+        {
+            // Arrange
+            var request = new ArtistReleaseRequest
+            {
+                ArtistId = Guid.NewGuid(),
+                PageNumber = 1,
+                PageSize = 0
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "PageSize");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "PageSize must be greater than or equal to 1");
+        }
+    }
+}
---
+++ src/MusicExplorer.UnitTests/Validators/ArtistSearchRequestValidatorTests.cs
@@ -0,0 +1,175 @@
+﻿using MusicExplorer.Models.Request;
+using MusicExplorer.Validators;
+
+namespace MusicExplorer.UnitTests.Validators
+{
+    public class ArtistSearchRequestValidatorTests
+    {
+        private readonly ArtistSearchRequestValidator _validator;
+
+        public ArtistSearchRequestValidatorTests()
+        {
+            _validator = new ArtistSearchRequestValidator();
+        }
+
+        [Fact]
+        public void Validate_ValidSearchCriteria_ShouldPass()
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = "John",
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validate_NullOrEmptySearchCriteria_ShouldFail(string searchCriteria)
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = searchCriteria,
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "SearchCriteria");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "SearchCriteria cannot be null, empty, or whitespace");
+        }
+
+        [Theory]
+        [InlineData("ab")]
+        [InlineData("12")]
+        public void Validate_InvalidSearchCriteriaLength_ShouldFail(string searchCriteria)
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = searchCriteria,
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "SearchCriteria");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "SearchCriteria must be of length 3 or more");
+        }
+
+        [Theory]
+        [InlineData("John1")]
+        [InlineData("123")]
+        public void Validate_InvalidSearchCriteriaContainingNumbers_ShouldFail(string searchCriteria)
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = searchCriteria,
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "SearchCriteria");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "SearchCriteria cannot contain numbers");
+        }
+
+        [Fact]
+        public void Validate_ValidPageNumber_ShouldPass()
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = "John",
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_InvalidPageNumber_ShouldFail()
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = "John",
+                PageNumber = 0,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "PageNumber");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "PageNumber must be greater than or equal to 1");
+        }
+
+        [Fact]
+        public void Validate_ValidPageSize_ShouldPass()
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = "John",
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_InvalidPageSize_ShouldFail()
+        {
+            // Arrange
+            var request = new ArtistSearchRequest
+            {
+                SearchCriteria = "John",
+                PageNumber = 1,
+                PageSize = 0
+            };
+
+            // Act
+            var result = _validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, error => error.PropertyName == "PageSize");
+            Assert.Contains(result.Errors, error => error.ErrorMessage == "PageSize must be greater than or equal to 1");
+        }
+    }
+}
